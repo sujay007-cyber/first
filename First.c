33b2ba35c4cc6dde51e128682b405659aa6e0174
HighWayTo_HELL <80191568+sujay007-cyber@users.noreply.github.com>
2022-06-22 11:00:53 +0530

    Create First.c

---
+++ First.c
@@ -0,0 +1,237 @@
+#include<stdlib.h>
+
+#include<string.h>
+
+#include<stdio.h>
+
+char prod[3][10]={"A->aBa","B->bB","B->@"}, input[10],stack[25];
+
+int top=-1; int j=0,k,l;
+
+void push(char item)
+
+{
+
+	stack[++top]=item;}
+
+void pop()
+
+{
+
+	top=top-1;
+
+}
+
+void display()
+
+{
+
+	int j;
+
+	for(j=top;j>=0;j--)
+
+		printf("%c",stack[j]);
+
+}
+
+void stackpush(char p)
+
+{
+
+	if(p=='A')
+
+	{
+
+		pop();
+
+		for(j=strlen(prod[0])-1;j>=3;j--)
+
+			push(prod[0][j]);
+
+	}
+
+	else
+
+	{
+
+		pop();
+
+		for(j=strlen(prod[1])-1;j>=3;j--)
+
+			push(prod[1][j]);
+
+	}
+
+}
+
+void main()
+
+{
+
+	char c; int i;
+
+	printf("\nGrammar\n");
+
+	printf("---------\n");
+
+    for(i=0;i<3;i++)
+
+    printf("%s\n",prod[i]);
+
+    printf("\n");
+
+	printf("first(A)={a}\n");
+
+	printf("follow(A)={$}\n");
+
+	printf("\nfirst(B)={b,@}\n");
+
+	printf("follow(B)={a}\n\n");
+
+	printf("a \tb \t$ \n");
+
+	printf("-----------------------\n");
+
+	printf("A\t%s\n",prod[0]);
+
+	printf("B\t%s\t%s\n",prod[2],prod[1]);
+
+	printf("\nenter the input string terminated with $ to parse:-");
+
+	scanf("%s",input);
+
+	for(i=0;input[i]!='\0';i++)
+
+	{
+
+		if((input[i]!='a')&&(input[i]!='b')&&(input[i]!='$'))
+
+		{
+
+			printf("invalid string");
+
+			exit(0);
+
+		}
+
+	}
+
+	if(input[i-1]!='$')
+
+	{
+
+		printf("\n\nInput string entered without end marker $");
+
+		exit(0);
+
+	}
+
+	push('$');
+
+	push('A');
+
+	i=0;
+
+	printf("\n\n");
+
+	printf("stack\tInput\tAction");
+
+	printf("\n---------------------------------------");
+
+	while(i!=strlen(input)&&stack[top]!='$')
+
+	{
+
+		printf("\n");
+
+		for(l=top;l>=0;l--)
+
+			printf("%c",stack[l]);
+
+		printf("\t");
+
+		for(l=i;l<strlen(input);l++)
+
+			printf("%c",input[l]);
+
+		printf("\t");
+
+		if(stack[top]=='A')
+
+		{
+
+			printf("A->aBa");
+
+			stackpush('A');
+
+		}
+
+		else if(stack[top]=='B')
+
+		{
+
+			if(input[i]!='b')
+
+			{
+
+				printf("B->@");
+
+				printf("\tmatched @");
+
+				pop();
+
+			}
+
+			else
+
+			{
+
+				printf("B->bB");
+
+				stackpush('B');
+
+			}
+
+		}
+
+		else
+
+		{
+
+			if(stack[top]==input[i])
+
+			{
+
+				printf("pop %c",input[i]);
+
+				printf("\tmatched %c",input[i]);
+
+				pop();
+
+				i++;
+
+			}
+
+			else
+
+				break;
+
+		}
+
+	}
+
+	if(stack[top]=='$' && input[i]=='$')
+
+	{
+
+		printf("\n$\t$\n");
+
+		printf("\nValid string Accepted\n");
+
+	}
+
+	else
+
+		printf("\nInvalid string rejected\n"); 
+
+}
